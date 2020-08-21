0772e423e3df631650cbbf311616e79c95f6e757
FRAZ5094 <60670235+FRAZ5094@users.noreply.github.com>
2020-08-21 21:49:36 +0100

    changing sleeps

--- motor.py
+++ motor.py
@@ -60,17 +60,17 @@ try:
         if char == ord("q"):
             break
         elif char == curses.KEY_UP:
-            forward(0.5)
-            sleep(0.5)
+            forward(1)
+            sleep(1)
         elif char == curses.KEY_DOWN:
-            backward(0.5)
-            sleep(0.5)
+            backward(1)
+            sleep(1)
         elif char == curses.KEY_LEFT:
-            left(0.4)
-            sleep(0.4)
+            left(1)
+            sleep(1)
         elif char == curses.KEY_RIGHT:
-            right(0.4)
-            sleep(0.4)
+            right(1)
+            sleep(1)
 finally:
     print("exited")
     curses.nocbreak(); screen.keypad(0); curses.echo()
