187f84e2e27b58575d214d2e67dffa541f9dada7
FRAZ5094 <60670235+FRAZ5094@users.noreply.github.com>
2020-08-21 21:27:15 +0100

    changed to curses

--- motor.py
+++ motor.py
@@ -1,8 +1,15 @@
-import RPi.GPIO as gpio
+#import RPi.GPIO as gpio
 from time import sleep
 import keyboard
+import curses
 
+
+screen = curses.initscr()
+curses.noecho() 
+curses.cbreak()
+screen.keypad(True)
 gpio.setmode(gpio.BOARD)
+
 gpio.setwarnings(False)
 
 left_forward=16
@@ -17,6 +24,7 @@ gpio.setup(right_backward,gpio.OUT)
 
 def forward(time):
     print(f"forward for {time} second")
+    return
     gpio.output(left_forward,True)
     gpio.output(right_forward,True)
     sleep(time)
@@ -26,6 +34,7 @@ def forward(time):
 
 def backward(time):
     print(f"backward for {time} second")
+    return
     gpio.output(left_backward,True)
     gpio.output(right_backward,True)
     sleep(time)
@@ -34,6 +43,7 @@ def backward(time):
 
 def left(time):
     print(f"left for {time} second")
+    return
     gpio.output(left_backward,True)
     gpio.output(right_forward,True)
     sleep(time)
@@ -43,6 +53,7 @@ def left(time):
 
 def right(time):
     print(f"right for {time} second")
+    return
     gpio.output(left_forward,True)
     gpio.output(right_backward,True)
     time(sleep)
@@ -51,20 +62,22 @@ def right(time):
 
 try:
     while True:
-        print(keyboard.read_key())
-        if keyboard.is_pressed('esc'):
+        char = screen.getch()
+        if char == ord('esc'):
             break
-        elif keyboard.is_pressed("up"):
+        elif char == curses.KEY_UP:
             forward(1)
             sleep(0.5)
-        elif keyboard.is_pressed("down"):
+        elif char == curses.KEY_DOWN:
             backward(1)
             sleep(0.5)
-        elif keyboard.is_pressed("left"):
+        elif char == curses.KEY_LEFT:
             left(1)
             sleep(0.5)
-        elif keyboard.is_pressed("right"):
+        elif char == curses.KEY_RIGHT:
             right(1)
             sleep(0.5)
 finally:
     print("exited")
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
